a817e5818579a79887048eafca237313ab48d9ea
LukaBazooka <102615598+LukaBazooka@users.noreply.github.com>
2024-12-21 17:03:29 -0500

    IntroCustcene Start

---
+++ Assets/IntroCutscene.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroCutscene : MonoBehaviour
+{
+    public GameObject BlackScreen;
+    public GameObject IntroText;
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
