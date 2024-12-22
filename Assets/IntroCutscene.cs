eed6613c2e0a45f88fbfc9423057e5e8e313af05
LukaBazooka <102615598+LukaBazooka@users.noreply.github.com>
2024-12-21 20:06:52 -0500

    IntroCutscene Stuff

--- Assets/IntroCutscene.cs
+++ Assets/IntroCutscene.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class IntroCutscene : MonoBehaviour
 {
-    public GameObject BlackScreen;
-    public GameObject IntroText;
+    [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] private CanvasGroup canvasGroup; 
 
-    void Start()
+    private void Start()
     {
-        
+        text.gameObject.SetActive(false);
+        StartCoroutine(TextAndImageRoutine());
     }
-    void Update()
+
+    private IEnumerator TextAndImageRoutine()
     {
+        yield return new WaitForSeconds(5f);
+
+        text.gameObject.SetActive(true);
+
+        yield return new WaitForSeconds(5f);
+
+        float fadeDuration = 1f;
+        float elapsedTime = 0f;
         
+        canvasGroup.alpha = 1f; 
+
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            float alpha = Mathf.Lerp(1f, 0f, elapsedTime / fadeDuration);
+            canvasGroup.alpha = alpha;
+            yield return null;  
+        }
+        
+        canvasGroup.alpha = 0f;
     }
 }
+
