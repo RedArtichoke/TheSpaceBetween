9c3ab10334b3ca21751f1d06651d7b226bc256d0
ArcadeSki <shaynelord@cmail.carleton.ca>
2025-01-10 00:11:05 -0500

    light flickering code done

--- Assets/Scripts/FlickerLight.cs
+++ Assets/Scripts/FlickerLight.cs
@@ -4,18 +4,45 @@ using UnityEngine;
 
 public class FlickerLight : MonoBehaviour
 {
-    float brightness;
+    public float brightness;
+    public float onThreshold;
+
+    public float holdTime;
+
+    Light myLite;
 
     // Start is called before the first frame update
     void Start()
     {
-        brightness = 0;
+        holdTime = 0.0f;
+        brightness = 5f;
+
+        myLite = GetComponent<Light>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Random.Range(0.0f, 0.5f);
+        if (holdTime > 0.1f)
+        {
+            onThreshold = Random.Range(0.0f, 1f);
+
+            if (onThreshold > 0.9f)
+            {
+                myLite.intensity = brightness;
+
+            }
+            else if (onThreshold > 0.7f)
+            {
+                myLite.intensity = brightness / 2f;
+            }
+            else
+            {
+                myLite.intensity = 0.0f;
+            }
+            holdTime = 0.0f;
+        }
+        holdTime += Time.deltaTime;
         //turning on and off after a random amount of time to seem like flickering
     }
 }
