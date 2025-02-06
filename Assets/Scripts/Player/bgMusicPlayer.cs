5978112bb1edcd7ab7567d1b10d5c0c5cd4216a8
Blake <bcyze@icloud.com>
2025-02-06 15:59:47 -0500

    Music Player Added ingame
    - three new tracks made by Mike Cyze
    - when an enemy is visible, cancels music
    - makes music quieter while suit is speaking
    - music plays intermittently with a varying cooldown

---
+++ Assets/Scripts/Player/bgMusicPlayer.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bgMusicPlayer : MonoBehaviour
+{
+    public AudioClip[] audioTracks; // Array to hold audio tracks
+    private AudioSource audioSource; // Audio source for playing music
+    private float originalVolume; // Store original volume
+    private float delayTime; // Time to wait before playing next track
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = gameObject.AddComponent<AudioSource>(); // Create audio source
+        originalVolume = audioSource.volume; // Store original volume
+        StartCoroutine(PlayRandomTrack()); // Start playing tracks
+    }
+
+    // Coroutine to play tracks with random delay
+    private IEnumerator PlayRandomTrack()
+    {
+        while (true)
+        {
+            delayTime = Random.Range(60f, 120f); // Random delay between 1-2 minutes
+            yield return new WaitForSeconds(delayTime); // Wait for delay
+            PlayTrack(); // Play a random track
+            yield return new WaitWhile(() => audioSource.isPlaying); // Wait for track to finish
+        }
+    }
+
+    // Play a random track
+    private void PlayTrack()
+    {
+        int trackIndex = Random.Range(0, audioTracks.Length); // Random track index
+        audioSource.clip = audioTracks[trackIndex]; // Set audio clip
+        audioSource.Play(); // Play audio
+    }
+
+    // Public method to cancel music and reset timer
+    public void CancelMusic()
+    {
+        audioSource.Stop(); // Stop current track
+        StopAllCoroutines(); // Stop coroutine
+        StartCoroutine(PlayRandomTrack()); // Restart coroutine with reset timer
+    }
+
+    // Public method to halve the volume
+    public void HalveVolume()
+    {
+        audioSource.volume = originalVolume / 2; // Halve volume
+    }
+
+    // Public method to reset volume to original
+    public void ResetVolume()
+    {
+        audioSource.volume = originalVolume; // Reset volume
+    }
+}
