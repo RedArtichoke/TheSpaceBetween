c97dfc3f7513161e407b39d2c9c080fdb7226963
LukaBazooka <102615598+LukaBazooka@users.noreply.github.com>
2025-02-07 17:25:22 -0500

    Sludge fix

--- Assets/Sludge.cs
+++ Assets/Sludge.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class Sludge : MonoBehaviour
 {
     public DarkController darkController;
-    
+
+    public GameObject particles;
+
+    public MeshRenderer meshRenderer;
+
+    public BoxCollider boxCollider;
     void Update()
     {
         if(darkController.inDark)
         {
-            gameObject.SetActive(true);
+            particles.SetActive(true);
+            meshRenderer.enabled = true;
+            boxCollider.enabled = true;
         }
         else
         {
-            gameObject.SetActive(false);
+            particles.SetActive(false);
+            meshRenderer.enabled = false;
+            boxCollider.enabled = false;
         }
     }
 }
