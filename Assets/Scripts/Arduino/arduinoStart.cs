03ef66bbc5b5d83313bd40e1042a173be3278fe6
musalinii <124927708+musalinii@users.noreply.github.com>
2025-02-21 17:02:28 -0500

    arduino intro done

--- Assets/Scripts/Arduino/arduinoStart.cs
+++ Assets/Scripts/Arduino/arduinoStart.cs
@@ -12,22 +12,37 @@ public class arduinoStart : MonoBehaviour
     public GameObject noArduinoUI;
     public GameObject callibrationUI;
 
-    private bool hideArduinoUI = false;
+    public GameObject breathingCircle;
 
+    private bool hideArduinoUI = false;
+    private Vector3 originalScale;
+    public float minScale = 0.8f;
+    public float maxScale = 1.2f;
+    public float breathingSpeed = 2.0f;
+    private bool isBreathing = false;
+
+    public GameObject introText;
+    public GameObject introCalibrationText;
+    public GameObject calibrationEnd;
+
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        originalScale = breathingCircle.transform.localScale;
+
+        StartCoroutine(startArduino());
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if (serialControllerScript.arduinoConnected == true && hideArduinoUI == false)
+       
+        if (isBreathing && breathingCircle != null)
         {
-            connectArduinoUI.SetActive(true);
+            BreathingEffect();
         }
 
     }
@@ -41,13 +56,74 @@ public class arduinoStart : MonoBehaviour
 
     public void callibration()
     {
-
-        hideArduinoUI = true;
-        connectArduinoUI.SetActive(false);
+        StartCoroutine(calibrationStop());
         callibrationUI.SetActive(true);
 
         arduinoControllerScript.StartCalibration();
+
+        isBreathing = true;
     }
+
+    public void StopCalibration()
+    {
+        isBreathing = false;  // Stop breathing effect
+        if (breathingCircle != null)
+        {
+            breathingCircle.transform.localScale = originalScale; // Reset scale
+        }
+
+        callibrationUI.SetActive(false);
+
+    }
+
+    private void BreathingEffect()
+    {
+        float scale = Mathf.Lerp(minScale, maxScale, (Mathf.Sin(Time.time * breathingSpeed) + 1) / 2);
+        breathingCircle.transform.localScale = originalScale * scale;
+    }
+
+    public void calibrationStart()
+    {
+        hideArduinoUI = true;
+        connectArduinoUI.SetActive(false);
+
+        introCalibrationText.SetActive(true);
+
+        StartCoroutine(callibrationIntro());
+    }
+
+    private IEnumerator startArduino()
+    {
+        yield return new WaitForSeconds(10);
+        introText.SetActive(false);
+        if (serialControllerScript.arduinoConnected == true && hideArduinoUI == false)
+        {
+            connectArduinoUI.SetActive(true);
+        }
+        else
+        {
+            noArduino();
+        }
+
+    }
+
+    private IEnumerator callibrationIntro()
+    {
+        yield return new WaitForSeconds(5);
+        introCalibrationText.SetActive(false);
+        callibration();
+
+    }
+
+    private IEnumerator calibrationStop()
+    {
+        yield return new WaitForSeconds(15);
+        StopCalibration();
+        calibrationEnd.SetActive(true);
+
+
+    }
+
 
 
 }
