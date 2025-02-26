bdb65880e15f434f9ce8989cec8a1779f2f51a70
LukaBazooka <102615598+LukaBazooka@users.noreply.github.com>
2025-02-26 18:50:52 -0500

    GameManager

--- Assets/DestroyShipTrigger.cs
+++ Assets/DestroyShipTrigger.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class DestroyShipTrigger : MonoBehaviour
 {
     public ShipRepair ship;
+
+    public GameObject triggerZone;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             ship.DestroyShip();
+            triggerZone.SetActive(true);
             Destroy(gameObject);
         }
     }
