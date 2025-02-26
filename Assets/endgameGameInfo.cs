54e2025f77093d77e306b7890324e21384ea77c8
ArcadeSki <shaynelord@cmail.carleton.ca>
2025-02-26 16:26:50 -0500

    heart rate post game info test

---
+++ Assets/endgameGameInfo.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class endgameGameInfo : MonoBehaviour
+{
+    [SerializeField] HeartRateAnimator heartinfo;
+
+    [SerializeField] float allBPM;      //used for "averageBPM" calculation
+    [SerializeField] float averageBPM;  //Average heart rate throughout the playthrough
+    [SerializeField] float peakBPM;     //highest heart rate throughout the playthrough
+
+    public float between;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        allBPM = 0.0f;
+        averageBPM = 0.0f;
+        peakBPM = 0.0f;
+
+        between = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(between > 1)
+        {
+            between = 0;
+
+            allBPM += heartinfo.beatsPerMinute;
+
+            averageBPM = allBPM / Time.fixedTime;
+
+            //if current heart rate is higher than the previous peak
+            if (heartinfo.beatsPerMinute > peakBPM)
+            {
+                peakBPM = heartinfo.beatsPerMinute;
+            }
+        }
+        between += Time.deltaTime;
+    }
+}
