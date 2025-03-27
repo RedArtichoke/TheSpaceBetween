7a4eb71b6681c44b3d7b298dc16f9fb92e2bdeb4
ArcadeSki <shaynelord@cmail.carleton.ca>
2025-03-27 19:28:12 -0400

    stall audiosource. sound next push

--- Assets/Scripts/WashroomStall.cs
+++ Assets/Scripts/WashroomStall.cs
@@ -7,25 +7,37 @@ public class WashroomStall : MonoBehaviour
     public bool isClosed = true;
     public void useDoor(Transform stallDoor)
     {
-        Animator hinge = stallDoor.parent.GetComponent<Animator>();
-        if(hinge == null)
+        Animator hinge = stallDoor.GetComponent<Animator>();
+        AudioSource[] doorAudio = stallDoor.GetComponents<AudioSource>();
+
+        if (hinge == null)
         {
-            return; //do not continue if there is no animator
+            hinge = stallDoor.parent.GetComponent<Animator>();
         }
-        Debug.Log("stall 2");
+        //Debug.Log("stall 2");
 
         if (hinge.GetBool("closed"))
         {
-            Debug.Log("stall 3");
+            //Debug.Log("stall 3");
             hinge.SetBool("closed", false);
             isClosed = false;
+
+            if (doorAudio[0] != null)
+            {
+                doorAudio[0].Play(); //open audio
+            }
         }
         else
         {
-            Debug.Log("stall 4");
+            //Debug.Log("stall 4");
             hinge.SetBool("closed", true);
             isClosed = true;
+
+            if (doorAudio[1] != null)
+            {
+                doorAudio[1].Play(); //close audio
+            }
         }
-        
+
     }
 }
